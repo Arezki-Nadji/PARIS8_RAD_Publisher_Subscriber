1ec93d5582f38a0a1c7f9dba607d72472ec6a906
AREZKI <arezki.m.nadji96@gmail.com>
2021-11-28 00:29:17 +0100

    Talker float PY

--- src/beginner_tutorial/scripts/talker.py
+++ src/beginner_tutorial/scripts/talker.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64
 
 def PublishMethod():
-	pub = rospy.Publisher('talker',String,queue_size=10) #defining the publisher by topic message type
+	pub = rospy.Publisher('talker',Float64,queue_size=10) #defining the publisher by topic message type
 	rospy.init_node('publish_node',anonymous = True) #defining the ros node -publisher node
 	rate = rospy.Rate(10) #10hz #frequency at witch the publishing
 	while not rospy.is_shutdown():
-		publishingString = "THis is being published" #variable
+		publishingString = 32  #variable
 		rospy.loginfo("DAta is being sent") #to print on the terminal
 		pub.publish(publishingString) #publishin
 if __name__=='__main__':
